59183f8d10417b04f1993ed39aa9b4f9e4900d0e
taro0513 <hank93513@gmail.com>
2024-08-06 11:38:10 +0800

    feat: add MIT license with module docstring

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,1 +1,20 @@
-__all__ = ["udn_scraper"]
+"""
+News Scraper Package
+
+This package provides classes and methods for scraping news articles from various news websites.
+The core functionality is encapsulated in the `NewsScraperBase` abstract class, which defines the
+interface for news scrapers. Implementations of this interface, such as the `UDNScraper`, provide
+concrete methods for fetching, parsing, and saving news articles.
+
+"""
+
+__author__ = "Intro to Software Eng. and Design Pat. with application to Project Develop Team"
+__copyright__ = "Copyright (c) 2024 Intro to Software Eng. and Design Pat. with application to Project Develop Team"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__email__ = "hank93513@gmail.com"
+
+from .base import NewsScraperBase, Headline, News
+from .udn_scraper import UDNScraper
+from .exceptions import DomainMismatchException
+
